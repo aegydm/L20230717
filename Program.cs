7ef16e865b18cf3dd76c1ba850859b84347236ce
peullepeu <aaaaydm3148@gmail.com>
2023-07-17 17:09:11 +0900

    ref, out

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,23 @@ namespace L20230717
 {
     internal class Program
     {
+
+        //sum, avg 
+
+        static void Calcualate(int a , int b, out int sum, out float avg)
+        {
+            sum = a + b;
+            avg = (float)sum / 2.0f;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            int a = 3;
+            int b = 4;
+            int sum = 0;
+            float avg = 0;
+            Calcualate(a, b, out sum, out avg);
+            Console.WriteLine(sum + " & " + avg);
+
         }
-    }
+    } 
 }
